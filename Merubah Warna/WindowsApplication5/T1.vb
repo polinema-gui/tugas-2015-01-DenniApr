eb82f2c5ed4bf2e3f4c8aa94dc00749ecfa62f2b
Deni Aprianto <denniapr12@gmail.com>
2015-10-02 14:45:34 +0700

    Merubah Warna

---
+++ Merubah Warna/WindowsApplication5/T1.vb
@@ -0,0 +1,21 @@
+﻿Public Class T1
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        Me.BackColor = Color.Red
+
+    End Sub
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Me.BackColor = Color.Red Then
+            Me.BackColor = Color.Aqua
+        Else
+            Me.BackColor = Color.Red
+        End If
+    End Sub
+End Class
+
